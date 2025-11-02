a140696085a2b83fdfcb01528ea4d3b6b6b2401e
Toni Peter Gugic <tonipetergugic@yahoo.com>
2025-11-02 10:55:03 +0100

    Cleanup: remove old artist upload page, optional create page, add root redirect, delete empty hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,5 @@
-"use client";
-
-import Link from "next/link";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/home");
-  }, [router]);
-
-  return (
-    <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
-      <div style={{ flex: 1 }} />
-      <div style={{ textAlign: "center", padding: "16px" }}>
-        <Link href="/artist/login" style={{ color: "#00FFC6" }}>
-          Are you an artist? Join IndieSound for Artists
-        </Link>
-      </div>
-    </div>
-  );
+  redirect("/home");
 }
